5a85efab0593ac23d4b14e4bb5f2cce7ad7dc28e
Lee-42 <1725210731@qq.com>
2022-07-09 22:53:31 +0800

    简单编写mini播放器

--- src/components/MiniPlayer/index.vue
+++ src/components/MiniPlayer/index.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="mini-player">mini播放器</div>
+  <div class="mini-player">
+    <div class="album-cover" @click="openMainPlayer">
+      <img src="../../assets/images/album_cover.webp" alt="" />
+    </div>
+    <div class="player-opra">
+      <i class="icon-iov-pre"></i>
+      <i class="icon-iov-pause"></i>
+      <i class="icon-iov-next"></i>
+    </div>
+    <div class="player-tool">
+      <i class="icon-shunxubofang-copy"></i>
+      <i class="icon-bofangduilie"></i>
+      <span>词</span>
+      <i class="icon-soound-min-full"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const openMainPlayer = () => {
+      console.log("openMainPlayer");
+    };
+
+    return {
+      openMainPlayer,
+    };
+  },
 });
 </script>
 
 <style lang="less" scoped>
 .mini-player {
   height: 60px;
-  background: #999999;
+  background: rgb(28, 28, 28);
+  padding: 10px;
+  display: flex;
+  align-items: center;
+
+  .album-cover {
+    img {
+      height: 40px;
+      border-radius: 5px;
+    }
+  }
+  .player-opra {
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    i {
+      color: rgb(199, 37, 38);
+      font-size: 24px;
+      margin: 10px;
+    }
+    i:nth-child(2) {
+      font-size: 40px;
+    }
+  }
+  .player-tool {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    right: 0;
+    i {
+      color: rgb(176, 176, 176);
+      font-size: 18px;
+      margin: 15px;
+    }
+    span {
+      color: rgb(176, 176, 176);
+      font-size: 18px;
+      margin: 15px;
+    }
+  }
 }
 </style>
--- src/views/LocalMusic.vue
+++ src/views/LocalMusic.vue
@@ -1,27 +1,65 @@
 <template>
   <div class="local-music">
-    <PlayListBtn
-      @play="handlePlay"
-      @add-to-list="handleAddToList"
-    ></PlayListBtn>
+    <a-table
+      :columns="columns"
+      :data-source="data"
+      bordered
+      size="small"
+      :pagination="false"
+      sticky
+    >
+      <template #bodyCell="{ column, text }">
+        <template v-if="column.dataIndex === 'name'">
+          <a>{{ text }}</a>
+        </template>
+      </template>
+      <template #title>Header</template>
+    </a-table>
   </div>
 </template>
-<script>
-import PlayListBtn from "../components/Base/PlayListBtn";
 
-export default {
-  components: { PlayListBtn },
+<script>
+import { defineComponent } from "vue";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Cash Assets",
+    dataIndex: "money",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    width: 100,
+    ellipsis: true,
+  },
+];
+const data = () => {
+  let arr = [];
+  for (let i = 0; i < 100; i++) {
+    arr.push({
+      key: i,
+      name: "John Brown",
+      money: "￥300,000.00",
+      address: "New York No. 1 Lake Park",
+    });
+  }
+  return arr;
+};
+export default defineComponent({
   setup() {
-    const handlePlay = () => {
-      console.log("handlePlay");
-    };
-    const handleAddToList = () => {
-      console.log("handleAddToList");
-    };
     return {
-      handlePlay,
-      handleAddToList,
+      data: data(),
+      columns,
     };
   },
-};
+});
 </script>
+<style>
+th.column-money,
+td.column-money {
+  text-align: right !important;
+}
+</style>
