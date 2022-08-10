0f6a3d923fc432a6700b1a4321c12f08ffb652fd
Lee-42 <1725210731@qq.com>
2022-08-10 14:06:21 +0800

    no message

---
+++ src/views/3dmodal.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="3dmodal">
+    <canvas
+      class="hero__model-canvas"
+      width="1565"
+      height="1335"
+      style="width: 626px; height: 534px"
+    ></canvas>
+  </div>
+</template>
